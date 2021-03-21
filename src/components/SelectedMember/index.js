b62500d5695d7331cf7496d93e3222c9f0958508
Lais Vitoria <laisvitoria0607@gmail.com>
2021-03-21 15:52:37 -0300

    style(master): estilização do selectedMember

--- src/components/SelectedMember/index.js
+++ src/components/SelectedMember/index.js
@@ -1,17 +1,19 @@
+import './styles.css'
+
 export default function SelectedMember({avatar, login, name, repositories, followers, created_at}){
     return (
         <div className="container__selectedMember">
+            <img src={avatar} alt={login}/>
             <div>
-                <img src={avatar} alt={login}/>
                 <div>
                     <h2>{name}</h2>
                     <h3>{login}</h3>
                 </div>
-            </div>
-            <div>
-                <p>{repositories}</p>
-                <p>{followers}</p>
-                <p>{created_at}</p>
+                <div>
+                    <p>Repositórios: {repositories}</p>
+                    <p>Seguidores: {followers}</p>
+                    <p>Entrou em: {created_at}</p>
+                </div>
             </div>
         </div>
     )
